c1e83daf83fda5a5a9a94ad7992b0003b7203301
Felipe <projetointernet@projetointernet.com.br>
2019-11-25 20:24:04 -0300

    novo projeto

---
+++ pages/cadastro.vue
@@ -0,0 +1,64 @@
+<template>
+<div>
+
+<v-container class="top-base-register">
+<div>
+    <div>
+     <a href="/"><p>voltar</p></a>
+    </div>
+
+    <center>
+        <form action ="/minha-area" style="width: 50%;">
+        <h1 class="section-title-login">Cadastre-se!</h1> 
+        <br>
+        <v-text-field class="label-form" v-model="nome" :error-messages="emailErrors" label="Nome"></v-text-field>
+        <v-text-field class="label-form" v-model="email" :error-messages="emailErrors" label="E-mail"></v-text-field>
+        <v-text-field class="label-form" v-model="telefone" :error-messages="emailErrors" label="Telefone"></v-text-field>
+        <v-text-field class="label-form" v-model="endereco" :error-messages="emailErrors" label="Endereço"></v-text-field>
+        <br>
+        
+        <v-row align="left" justify="space-around" style="margin-left: 286px; color: black !important;">
+            <v-icon>fas fa-lock</v-icon>
+        </v-row>
+
+        <v-text-field type="password" class="label-form" v-model="senha" :error-messages="emailErrors" label="Senha"></v-text-field>
+        <v-btn to="/minha-area"  style="border: 0; color: #FFF;border-radius: 60px;background-color: #451F55;width: 436px;height: 50px;font-size: 25px!important; text-transform: none;">FINALIZAR</v-btn>
+        
+        </form>
+    </center>
+  </div>
+  </v-container>
+</div>
+</template>
+
+<script>
+     const axios = require('axios')
+    export default {
+        data(){
+            return{
+                nome: '',
+                email: '',
+                telefone: '',
+                endereco: '',
+                senha: ''
+            }    
+        },
+        
+       methods: {
+            register: function(e){
+                const parametros = new URLSearchParams();
+                parametros.append("nome", this.nome);
+                parametros.append("email", this.email);
+                parametros.append("telefone", this.telefone);
+                parametros.append("endereco", this.endereco);
+                parametros.append("senha", this.senha);
+
+                axios.post('http://localhost:9000/registration', parametros);
+            }
+        }
+    }
+
+    const routes = [
+  { path: '/minha-area' }
+];
+</script>
---
+++ pages/localizar.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    <v-container class="page-header p-t-70">
+    <h2 class="internal-title">Escolha o profissional ideal!</h2> 
+    </v-container>
+
+    <v-container >
+        <div class="p-first">
+            <div class="one-p">
+                <div class="one-left">
+                    <img src="/images/placeholder-p.png" width="130px" alt="">
+                </div>
+                <div class="one-right">
+                    <p>Nome</p>
+                    <p>Titulo</p>
+                    <v-btn class="p-perfil">Ver Perfil</v-btn>
+                    <v-btn class="p-escolher">Escolher</v-btn>
+                </div>
+            </div>
+
+            <div class="two-p">
+                 <div class="two-left">
+                    <img src="/images/placeholder-p.png" width="130px" alt="">
+                </div>
+                <div class="two-right">
+                    <p>Nome</p>
+                    <p>Titulo</p>
+                    <v-btn class="p-perfil">Ver Perfil</v-btn>
+                    <v-btn class="p-escolher">Escolher</v-btn>
+                </div>
+            </div>
+        </div>
+    </v-container>
+</div>
+</template>
